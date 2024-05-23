6fe30a717f8876daf2901c651e0edfefd58f7eb2
Chinh Do <89497629+Chid0k@users.noreply.github.com>
2024-05-23 20:58:07 +0700

    Delete CryptoHack directory

--- CryptoHack/DIFFIE-HELLMAN/STARTER/Starter 1.py
+++
@@ -1,4 +0,0 @@
-from Crypto.Util.number import *
-g = 209
-d = inverse(g, 991)
-print(d)
--- CryptoHack/MODULAR_ARITHMETIC/Modular_Inverting.py
+++
@@ -1,9 +0,0 @@
-# 3 * d = 1 [13]
-
-print(pow(3, -1, 13))
-
-# Calculator
-# Fermat pow (a, n - 1) = 1 [n]
-# d = pow(3, 11)
-print(pow(3, 11) % 13)
-
